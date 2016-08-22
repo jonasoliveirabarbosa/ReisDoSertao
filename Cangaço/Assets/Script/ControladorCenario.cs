cd84ec68d6046edd1fef8c8e0ff093ac6f25c3b5
jonasoliveirabarbosa <jonasoliveirabarbosa@hotmail.com>
2016-08-21 22:15:25 -0300

    Primeira Publicação

---
+++ Cangaço/Assets/Script/ControladorCenario.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorCenario : MonoBehaviour {
+
+    public GameObject chao;
+    
+    void Start () {
+        float high = chao.GetComponent<SpriteRenderer>().bounds.size.x;
+        float whidth = chao.GetComponent<SpriteRenderer>().bounds.size.y;
+
+        for (int x = -10; x < 50; x++)
+        {
+            for (int y = -10; y < 10; y++)
+            {
+                GameObject instance = Instantiate(chao, new Vector3(x * high, y * whidth, 0f), Quaternion.identity) as GameObject;
+               
+            }
+        }
+    }
+}
